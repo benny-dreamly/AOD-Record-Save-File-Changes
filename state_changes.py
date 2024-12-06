4cc869874974327bbe163ac22a49c726dc1dcc5f
Benny D <bgaming0277@gmail.com>
2024-12-06 12:06:36 -0700

    remove old code

--- state_changes.py
+++ state_changes.py
@@ -5,13 +5,6 @@ with open('default_state.json') as raw_default_state:
 
 default_state_objects = default_state['game_objects']
 
-sample_object = default_state_objects[0]
-
-#print(sample_object)
-
-#print(sample_object['name'])
-#print(sample_object['carried'])
-
 default_status = [
     (dic['name'], dic['carried'])
     for dic in default_state_objects
@@ -33,13 +26,6 @@ changed_status = [
     if 'name' in dic and 'carried' in dic
 ]
 
-# for dic in changed_objects:
-#     if 'name' in dic and 'carried' in dic:
-#         item_name = dic['name']
-#         carried_item_status = dic['carried']
-#         item_status = [item_name, carried_item_status]
-#     changed_status.append(item_status)
-
 changed_item_status = set(changed_status)
 
 print('changed status\n\n', changed_item_status)
@@ -47,11 +33,3 @@ print('changed status\n\n', changed_item_status)
 changed_items = changed_item_status - default_item_status
 
 print(changed_items)
-
-#print(changed_state)
-
-#pairs = zip(default_state_objects, changed_state)
-
-#print([(default_state_objects, changed_state) for default_state_objects, changed_state in pairs if default_state_objects != changed_state])
-
-# set(default_state_objects).intersection(changed_state)
