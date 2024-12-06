b707b4e4fa523193d06003bf962aced5f66c61d9
Benny D <bgaming0277@gmail.com>
2024-12-06 12:14:04 -0700

    make save comparison a function

--- state_changes.py
+++ state_changes.py
@@ -1,5 +1,7 @@
 import json
 
+save_file = input('save file?')
+
 with open('default_save.json') as raw_default_state:
     default_state = json.load(raw_default_state)
 
@@ -15,21 +17,27 @@ default_item_status = set(default_status)
 
 # print("\n", "default status\n\n", default_item_status, "\n")
 
-with open('Scroll.json') as raw_changed_state:
-    changed_state = json.load(raw_changed_state)
+def compareSaves(save_file):
 
-changed_objects = changed_state['game_objects']
+    with open(save_file) as raw_changed_state:
+        changed_state = json.load(raw_changed_state)
 
-changed_status = [
-    (dic['name'], dic['carried'])
-    for dic in changed_objects
-    if 'name' in dic and 'carried' in dic
-]
+    changed_objects = changed_state['game_objects']
 
-changed_item_status = set(changed_status)
+    changed_status = [
+        (dic['name'], dic['carried'])
+        for dic in changed_objects
+        if 'name' in dic and 'carried' in dic
+    ]
 
-# print('changed status\n\n', changed_item_status, '\n')
+    changed_item_status = set(changed_status)
 
-changed_items = changed_item_status - default_item_status
+    # print('changed status\n\n', changed_item_status, '\n')
+
+    changed_items = changed_item_status - default_item_status
+
+    return changed_items
+
+changed_items = compareSaves(save_file)
 
 print(changed_items)
