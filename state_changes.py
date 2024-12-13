b3c05f455c18bed9d788b17ba4e23322348a2a3c
Benny D <bgaming0277@gmail.com>
2024-12-13 11:36:51 -0700

    analyze new save data

--- state_changes.py
+++ state_changes.py
@@ -9,13 +9,13 @@ with open('default_save.json') as raw_default_state:
     default_state = json.load(raw_default_state)
 
 default_state_objects = default_state['game_objects']
-default_state_conditions = (default_state['door_open'],
-                            default_state['safe_open'],
-                            default_state['trapdoor_open'],
-                            default_state['fire_lit'],
-                            default_state['fire_extinguished'])
-
-print(default_state_conditions)
+default_conditions = {
+    ('door_open', default_state['door_open']),
+    ('safe_open', default_state['safe_open']),
+    ('trapdoor_open', default_state['trapdoor_open']),
+    ('fire_lit', default_state['fire_lit']),
+    ('fire_extinguished', default_state['fire_extinguished'])
+}
 
 default_status = [
     (dic['name'], dic['carried'])
@@ -27,11 +27,9 @@ default_item_status = set(default_status)
 
 # print("\n", "default status\n\n", default_item_status, "\n")
 
-def compare_saves(save):
+def compare_items(save):
 
-    save_file_path = get_save_file_path() / save
-    with open(save_file_path) as raw_changed_state:
-        changed_state = json.load(raw_changed_state)
+    changed_state = open_save(save)
 
     changed_objects = changed_state['game_objects']
     changed_conditions = (changed_state['door_open'],
@@ -54,6 +52,14 @@ def compare_saves(save):
 
     return difference
 
+
+def open_save(save):
+    save_file_path = get_save_file_path() / save
+    with open(save_file_path) as raw_changed_state:
+        changed_state = json.load(raw_changed_state)
+    return changed_state
+
+
 def get_save_file_path():
     system = platform.system()
     if system == "Windows":
@@ -65,8 +71,28 @@ def get_save_file_path():
     save_path.mkdir(parents=True, exist_ok=True)
     return save_path
 
+def compare_conditions(save):
+
+    changed_state = open_save(save)
+
+    changed_conditions = {
+        ('door_open', changed_state['door_open']),
+        ('safe_open', changed_state['safe_open']),
+        ('trapdoor_open', changed_state['trapdoor_open']),
+        ('fire_lit', changed_state['fire_lit']),
+        ('fire_extinguished', changed_state['fire_extinguished'])
+    }
+
+    condition_difference = changed_conditions - default_conditions
+
+    return condition_difference if condition_difference else None
 
 
-changed_items = compare_saves(save_file)
+changed_items = compare_items(save_file)
+changed_condition_state = compare_conditions(save_file)
 
 print(changed_items)
+if changed_condition_state is not None:
+    print(changed_condition_state)
+else:
+    pass
